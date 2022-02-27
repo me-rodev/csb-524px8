20215a47fcabdecd437aa61622313fa8d30eec90
Code Sandbox <hello@codesandbox.io>
2022-02-27 15:52:15 +0000

    Deployment from Codesandbox.

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -11,3 +11,4 @@ console.log(noOfElements([2, 3, 0, 1, 7], 4, 8));
 console.log(noOfElements([3, 3, 3, 5, 18, 6, 20], 15, 30));
 console.log(noOfElements([4, 6, 2, 6, 7, 7], 0, 7));
 module.exports = noOfElements;
+console.log("TEST");
